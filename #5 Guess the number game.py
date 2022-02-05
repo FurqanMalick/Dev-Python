4f4b09f4831cd06e1660eb66c733edd18f8028fd
Furqan Malick <furqanmalick00@gmail.com>
2022-02-05 14:04:36 +0500

    #5 Guess the number game "Update functions & Play Again"

--- #5 Guess the number game.py
+++ #5 Guess the number game.py
@@ -1,21 +1,35 @@
-n = 44
-no_of_guesses = 10
-print("Guess the number between 1 to 50")
-while (True):
-    if no_of_guesses == 0:
-        print("Game Over")
-        break
-    guess = int(input("Guess the number: "))
-    if guess > n:
-        print("Greater! Decrease Some")
-        no_of_guesses = no_of_guesses - 1
-        print("\n",no_of_guesses, "Attempt Left\n")
-    elif guess < n:
-        print("Lesser! Increase some")
-        no_of_guesses = no_of_guesses - 1
-        print("\n",no_of_guesses, "Attempt Left\n")
-        continue
+def game():
+    n = 44
+    no_of_guesses = 10
+    attempt = 0
+    print("Guess the number between 1 to 50\n")
+    while (True):
+        if no_of_guesses == 0:
+            print("Game Over")
+            break
+        guess = int(input("Guess the number: "))
+        if guess > n:
+            print("Greater! Decrease Some")
+            no_of_guesses = no_of_guesses - 1
+            attempt = attempt + 1
+            print("\n",no_of_guesses, "Attempt Left\n")
+            continue
+        if guess < n:
+            print("Lesser! Increase some")
+            no_of_guesses = no_of_guesses - 1
+            attempt = attempt + 1
+            print("\n",no_of_guesses, "Attempt Left\n")
+            continue
+        if guess == n:
+            print("Congratulations! You guess the hidden number in just", attempt, "Attempt\n")
+            break
+        return guess
+    again()
+def again():
+    play_again = input("Press \'y\'for Play Again: ")
+    play_again.upper()
+    if play_again == "y":
+        game()
     else:
-        print("Congratulations! You guess the hidden number in just", no_of_guesses, "Attempt")
-        break
-input("Press \'Y\' for Close")
+        exit()
+game()
